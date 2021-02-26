579e9255ebbb3733b75d219fee722ff0e9a61da1
avramandrei <avram.andreimarius@gmail.com>
2021-02-26 09:49:50 +0200

    Added everything to cuda

--- train.py
+++ train.py
@@ -20,14 +20,12 @@ def train_model(model, train_loader, dev_loader, optimizer, scheduler, criterion
         loss, f1 = 0, 0
 
         for i, (train_x, train_mask, train_y) in enumerate(train_tqdm):
-            if i > 2: break
-
             train_tqdm.set_description("Training - Epoch: {}/{}, Loss: {:.4f}, F1: {:.4f}".format(epoch + 1, args.epochs, loss, f1))
             train_tqdm.refresh()
 
             optimizer.zero_grad()
 
-            logits = model.forward(train_x, train_mask)
+            logits = model.forward(train_x.to(device), train_mask.to(device))
 
             loss = criterion(logits.to(device), train_y.to(device))
             loss.backward()
@@ -43,14 +41,12 @@ def train_model(model, train_loader, dev_loader, optimizer, scheduler, criterion
         model.eval()
 
         for i, (dev_x, dev_mask, dev_y) in enumerate(dev_tqdm):
-            if i > 2: break
-
             dev_tqdm.set_description("Evaluating - Epoch: {}/{}, Loss: {:.4f}, F1: {:.4f}".format(epoch + 1, args.epochs, loss, f1))
             dev_tqdm.refresh()
 
             optimizer.zero_grad()
 
-            logits = model.forward(dev_x, dev_mask)
+            logits = model.forward(dev_x.to(device), dev_mask.to(device))
 
             loss = criterion(logits.to(device), dev_y.to(device))
             loss.backward()
@@ -90,7 +86,7 @@ def train():
             print("\nTraining for language: '{}' using: '{}'...".format(lang, config[lang]))
 
             lang_model = AutoModel.from_pretrained(config[lang])
-            model = LangModelWithDense(lang_model, num_classes)
+            model = LangModelWithDense(lang_model, num_classes).to(device)
 
             optimizer = AdamW(model.parameters(), lr=3e-5)
 
@@ -108,7 +104,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str, default="configs/models.yml", help="Tokenizer used for each language.")
     parser.add_argument("--data_path", type=str, default="data/eurovoc", help="Path to the EuroVoc data.")
-    parser.add_argument("--epochs", type=int, default=5, help="Number of epochs to train the model.")
+    parser.add_argument("--epochs", type=int, default=1, help="Number of epochs to train the model.")
     parser.add_argument("--batch_size", type=int, default=2, help="Batch size of the dataset.")
     parser.add_argument("--device", type=str, default="cpu", help="Device to train on.")
     parser.add_argument("--save_path", type=str, default="models", help="Save path of the models")
