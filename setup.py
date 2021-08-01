e9d32b25a5bdef8bb30bb932a052e8850dc662a6
Andrei-Marius Avram <avram.andreimarius@gmail.com>
2021-08-01 13:29:30 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Alpha',
 
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
